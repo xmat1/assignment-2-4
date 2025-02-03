135d6eacc3ef7e481c0adeaae78122e2bc76a6de
x.mat1 <mat12345duq6789@gmail.com>
2025-02-02 22:15:20 -0500

    added taxes calculation

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # input statements
-salary = float(input())
-numDependents = float(input())
+salary = float(input("Enter salary: $"))
+numDependents = int(input("Enter number of dependents: "))
 
 # calculate taxes here
+stateTax = salary * 0.065
+federalTax = salary * 0.28
+dependentDeduction = numDependents * 0.025 * salary
+totalWithholding = stateTax + federalTax + dependentDeduction
+takeHomePay= salary - totalWithholding
 
 # output statements
+print("State Tax: $" + str(stateTax))
+print("Federal Tax: $" + str(federalTax))
+print("Dependents: $" + str(dependentDeduction))
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
